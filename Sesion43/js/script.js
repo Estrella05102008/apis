ac4d2471fc0321ee20661a14aa39f3c0ad555653
Estrella <samelyestrella41@gmail.com>
2024-09-03 19:44:59 -0500

    agregando archivos

---
+++ Sesion43/js/script.js
@@ -0,0 +1,12 @@
+let persona = {}
+
+persona.nombre = "Estrella";
+persona.edad = 15;
+persona.genero = "Femenino";
+
+let animal = {}
+
+animal.nombre = "Wolfdog";
+animal.personalidad = "Leal";
+animal.caracter = "Valiente";
+
